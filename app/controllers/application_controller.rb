666216bea8dcadd35ceeff40f69e278588e148a4
Mike White <michaeljwhite@gmail.com>
2020-02-15 15:06:44 -0800

    enable sessions, added and mounted controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, 'you_gotta_diversify_your_bonds'
   end
 
   get "/" do
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+
+    get '/login' do
+
+    end
+
+    get '/logout' do
+
+    end
+
+    post '/login' do
+        
+    end
+end
