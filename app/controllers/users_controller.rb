9ff519d4075743d88cbc3bd28bbf45082900c0c6
Mike White <michaeljwhite@gmail.com>
2020-02-15 16:43:13 -0800

    added and integrated logged_in? and current_user helpers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,31 @@
 class UsersController < ApplicationController
 
     get '/login' do
+        redirect to "/users/#{current_user.id}" if logged_in?
         erb :'users/login'
     end
 
     get '/logout' do
-
+        session.destroy if logged_in?
+        redirect to '/login'
     end
 
     get '/users/:id' do
-        @user = User.find(session[:user_id])
-        erb :'users/show'
+        if logged_in? && params[:id] == current_user.id
+          @user = User.find(session[:user_id])
+          erb :'users/show'
+        end
     end
 
     post '/login' do
-        @user = User.find_by(username: params[:username])
-        if @user && @user.authenticate(params[:password])
-            session[:user_id] = @user.id
-            redirect to "/users/#{@user.id}"
+        if !logged_in
+          user = User.find_by(username: params[:username])
+          if user && user.authenticate(params[:password])
+              session[:user_id] = user.id
+              redirect to "/users/#{user.id}"
+          end
+        else
+            redirect to '/logout'
         end
     end
 end
